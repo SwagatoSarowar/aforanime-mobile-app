f9d530549cd6d887415f1c0acf5dbef7646d6b34
SwagatoSarowar <swagotosarowar1@gmail.com>
2024-12-04 16:08:42 +0600

    fixed a bug

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -77,7 +77,7 @@ export default function Home() {
                   >
                     Welcome {user?.firstName}
                   </Text>
-                  <Link href="/(root)/profile">
+                  <Link href="/(root)/(tabs)/profile">
                     <Image
                       source={{ uri: user?.imageUrl }}
                       className="h-14 w-14 rounded-full border-2 border-dark-400"
